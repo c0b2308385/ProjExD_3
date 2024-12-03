86479a99ea7d5dd1c2589fee8fbb6e91d12d4584
Shoyama Kaito <C0B2308385@edu.teu.ac.jp>
2024-12-03 14:48:57 +0900

    練習4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -163,6 +163,9 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
@@ -172,7 +175,7 @@ def main():
                 bomb = None
                 bird.change_img(6, screen)
                 pg.display.update()
-
+ 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         beam.update(screen)
